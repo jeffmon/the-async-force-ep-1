c0988f1e1b1abf6f02d0d5ae1aa5a0dda7b0d8d4
jeffmon <jeffmon@gmail.com>
2017-06-08 17:31:36 -1000

    finished homeland for darth vader

--- js/app.js
+++ js/app.js
@@ -1,26 +1,46 @@
 (function (){
 
 
-  var targetPerson4 = document.getElementById("person4Name");
+  var targetPerson4Name = document.getElementById("person4Name");
+  var targetPerson4HomeWorld = document.getElementById("person4HomeWorld");
 
-  function reqListener(){
+  function reqListener1(){
 
     var response = JSON.parse(this.responseText);
 
-    var personBox = document.createElement("div");
-    personBox.innerHTML = response.name;
-    targetPerson4.appendChild(personBox);
+    var vaderName = document.createElement("div");
+    vaderName.innerHTML = response.name;
+    targetPerson4Name.appendChild(vaderName);
+
+  }
+
+  function reqListener2(){
+
+    var response = JSON.parse(this.responseText);
+
+    var vaderHomeWorld = document.createElement("div");
+    vaderHomeWorld.innerHTML = response.name;
+    targetPerson4HomeWorld.appendChild(vaderHomeWorld);
+    console.log(response);
 
   }
 
   function getPerson(id){
     var oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", reqListener);
+    oReq.addEventListener("load", reqListener1);
     oReq.open("GET", "http://www.swapi.co/api/people/" + id);
     oReq.send();
   }
 
+  function getPlanet(id){
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener2);
+    oReq.open("GET", "http://www.swapi.co/api/planets/" + id);
+    oReq.send();
+  }
+
   getPerson(4);
+  getPlanet(1);
 
 
 
