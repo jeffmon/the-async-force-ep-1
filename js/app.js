43d1f28fd41ab6106871334df4ae09cdb9317ddf
jeffmon <jeffmon@gmail.com>
2017-06-08 19:26:55 -1000

    finished ex

--- js/app.js
+++ js/app.js
@@ -53,13 +53,6 @@
     oReq.send();
   }
 
-  function getFilms(){
-    var oReq = new XMLHttpRequest();
-    oReq.addEventListener("load", reqListener4);
-    oReq.open("GET", "http://swapi.co/api/films/");
-    oReq.send();
-  }
-
   getPerson(4);
   getPlanet(1);
   getPerson(14);
@@ -67,19 +60,56 @@
 
   var targetFilmList = document.getElementById("filmList");
 
+  function getFilms(){
+    var oReq = new XMLHttpRequest();
+    oReq.addEventListener("load", reqListener4);
+    oReq.open("GET", "http://swapi.co/api/films/");
+    oReq.send();
+  }
+
   function reqListener4(){
     var response = JSON.parse(this.responseText);
 
-    console.log(response.results[0].title);
-
-    for(var i = 0; i < response.results.length; i++){
-      var film = document.createElement("li");
-      film.className = "film";
-      targetFilmList.appendChild(film);
+    for(var i = 0, len = response.results.length; i < len; i++){
+      var filmContainer = document.createElement("li");
+      filmContainer.className = "film";
+      targetFilmList.appendChild(filmContainer);
       var filmTitle = document.createElement("h2");
       filmTitle.innerHTML = response.results[i].title;
-      film.appendChild(filmTitle);
-    }
+      filmContainer.appendChild(filmTitle);
+      var planetHeading = document.createElement("h3");
+      filmContainer.appendChild(planetHeading);
+
+      var filmPlanetsContainer = document.createElement("ul");
+      filmPlanetsContainer.className = "filmPlanets";
+      filmContainer.appendChild(filmPlanetsContainer);
+
+      for(var p = 0; p < response.results[i].planets.length; p++){
+
+
+        (function(myPlanets){
+         var pReq = new XMLHttpRequest();
+         pReq.addEventListener("load", function(){
+            var pRes = JSON.parse(this.responseText);
+
+            var planetListItemContainer = document.createElement("li");
+           planetListItemContainer.className = "planet";
+
+           var planetTitleHeading = document.createElement("h4");
+           planetTitleHeading.className = "planetName";
+           planetTitleHeading.innerHTML = pRes.name;
+
+           planetListItemContainer.appendChild(planetTitleHeading);
+           myPlanets.appendChild(planetListItemContainer);
+          });
+          pReq.open("GET", response.results[i].planets[p]);
+          pReq.send();
+       })(filmPlanetsContainer);
+
+      }//for loop p
+
+
+    }//for loop i
 
   }
 
@@ -96,3 +126,4 @@
 
 
 })();
+
